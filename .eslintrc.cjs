20edff364a153332814cd40770e203db6dfc4c5b
Denis Obolevich <denzelwash33@mail.ru>
2023-04-20 00:07:30 +0300

    eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+	root: true,
+	extends: ['plugin:vue/vue3-essential', '@vue/eslint-config-prettier/skip-formatting'],
+	parserOptions: {
+		ecmaVersion: 'latest'
+	},
+	rules: {}
+}
