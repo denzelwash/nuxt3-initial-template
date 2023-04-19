c78b4d2a304dc6ebdb2b9fce128f145b55dce566
Denis Obolevich <denzelwash33@mail.ru>
2023-04-20 00:00:05 +0300

    Мета теги

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Project title',
+      meta: [
+        { name: 'description', content: '' },
+        { name: 'keywords', content: '' },
+        { property: 'og:locale', content: '' },
+        { property: 'og:type', content: '' },
+        { property: 'og:title', content: '' },
+        { property: 'og:description', content: '' },
+        { property: 'og:image', content: '' },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' },
+      ],
+    }
+  },
   modules: [
     '@pinia/nuxt',
   ],
