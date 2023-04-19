0e91c106568e5429d6df6c199218064c0b8b89ff
Denis Obolevich <denzelwash33@mail.ru>
2023-04-19 23:43:36 +0300

    Добавил pinia

---
+++ stores/base.js
@@ -0,0 +1,9 @@
+import { defineStore, acceptHMRUpdate } from 'pinia'
+
+export const useBase = defineStore('base', () => {
+  
+})
+
+if (import.meta.hot) {
+	import.meta.hot.accept(acceptHMRUpdate(useBase, import.meta.hot))
+}
